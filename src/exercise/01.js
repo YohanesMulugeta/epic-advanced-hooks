66da782a3e333f0cc1e4eb2dfeb66cc84ec93d57
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-16 11:08:47 +0300

    configure count reducer to be able to work based on the step passed it it

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,13 +4,13 @@
 import * as React from 'react'
 
 function countReducer(previousValue, newValue) {
-  return newValue
+  return previousValue + newValue
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [count, setCount] = React.useReducer(countReducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
