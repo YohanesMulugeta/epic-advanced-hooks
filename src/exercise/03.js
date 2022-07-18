e8b6e6e2b52c77fab16cfdc968aefc94feb46457
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-18 11:48:52 +0300

    create a cunsumer custom hook to hide the implementation of the the context and to handle errors caused when we try to use context outside context.provider

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -12,21 +12,25 @@ function CountProvider(props) {
 
   const value = [count, setCount]
 
-  //   🐨 create a `value` array with count and setCount
-  //   🐨 return your context provider with the value assigned to that array and forward all the other props
-  //   💰 more specifically, we need the children prop forwarded to the context provider
-
   return <IncrementContext.Provider value={value} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(IncrementContext)
+
+  if (!context) throw new Error('useCount must be used within a count provider')
+
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(IncrementContext)
+  const [count] = useCount()
 
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(IncrementContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
